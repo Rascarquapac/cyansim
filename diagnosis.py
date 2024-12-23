1d1c256b3bd6935b9a8bcd69f066b27ccbcf2931
Thierry <thierry.nancy@cyanview.com>
2024-12-23 08:40:29 +0100

    Using existing test case file

--- diagnosis.py
+++ diagnosis.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import json as json
 from pprint   import pprint
@@ -9,25 +10,30 @@ from view_network  import ViewNetwork
 from view_lens     import ViewLens
 from gear import Cyangear
 from draw_mermaid import Mermaid
+from message import Messages
 from draw     import Draw
 
 class Test():
 	def __init__(self):
+		self.filename   = "some_case.json"
 		self.descriptor = Descriptor(update=True)
 		self.camera     = ViewCamera(self.descriptor)
 		self.pool       = Pool()
-		self.case       = Case(camera=self.camera,pool= self.pool,active=True)
+		self.case       = Case(camera=self.camera,pool= self.pool,active=True,filename=self.filename)
 		self.cyangear   = Cyangear(self.pool)
 		self.network    = ViewNetwork(self.pool)
 		self.lens       = ViewLens(self.pool)
 		self.cyangear   = Cyangear(self.pool)
+		self.messages   = Messages()
 		self.draw       = Draw()
 if __name__ == "__main__":
 	test=Test()
-	test.camera.edit_number()
-	test.camera.display_selected()
-	test.network.edit()
-	test.lens.edit()
-	test.cyangear.analyze()
-	mermaid=Mermaid()    
-	mermaid.code(test.cyangear)
+	if os.path.exists(test.filename):
+		test.camera.edit_number()
+		test.camera.display_selected()
+		test.network.edit()
+		test.lens.edit()
+		test.cyangear.analyze()
+		print(test.messages.gear_list(test.cyangear))
+		mermaid=Mermaid()    
+		mermaid.code(test.cyangear)
