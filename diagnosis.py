bc81b79726452d742f1901fe44d6b45de5e9840e
Thierry <thierry.nancy@cyanview.com>
2024-12-24 09:14:19 +0100

    Changing json filepath for intiating case

--- diagnosis.py
+++ diagnosis.py
@@ -1,25 +1,29 @@
 import os
 import pandas as pd
 import json as json
-from pprint   import pprint
-from load import Descriptor
+from descriptor import Descriptor
 from pool import Pool
 from case import Case
 from view_camera   import ViewCamera
 from view_network  import ViewNetwork
 from view_lens     import ViewLens
-from gear import Cyangear
-from draw_mermaid import Mermaid
+from gear          import Cyangear
+from draw_mermaid  import Mermaid
 from message import Messages
 from draw     import Draw
 
 class Test():
 	def __init__(self):
-		self.filename   = "some_case.json"
-		self.descriptor = Descriptor(update=True)
+		# PARAMETERS
+		gsheet    = True
+		case_init = True
+		self.casefile   = "./data/xcase_some_case.json"
+		self.casefile   = "./data/xcase_initcase.json"
+		# OBJECTS
+		self.descriptor = Descriptor(updateFromGsheet=gsheet)
 		self.camera     = ViewCamera(self.descriptor)
 		self.pool       = Pool()
-		self.case       = Case(camera=self.camera,pool= self.pool,active=True,filename=self.filename)
+		self.case       = Case(camera=self.camera,pool= self.pool,active=case_init,filename=self.casefile)
 		self.cyangear   = Cyangear(self.pool)
 		self.network    = ViewNetwork(self.pool)
 		self.lens       = ViewLens(self.pool)
@@ -28,7 +32,7 @@ class Test():
 		self.draw       = Draw()
 if __name__ == "__main__":
 	test=Test()
-	if os.path.exists(test.filename):
+	if os.path.exists(test.casefile):
 		test.camera.edit_number()
 		test.camera.display_selected()
 		test.network.edit()
