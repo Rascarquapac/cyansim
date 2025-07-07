12b642d08b28420f368b0c34f7454b84dd4ae996
Thierry <thierry.nancy@cyanview.com>
2025-07-07 09:50:28 +0200

    Simplifying CameraLens

--- constants.py
+++ constants.py
@@ -28,52 +28,9 @@ class Camera(CameraProtocol):
 
 class CameraLens():
 	def __init__(self) -> None:
+        #TODO suppress the following lines and use Cameratype and CameraCategory in place of string values
 		self.camera_types = [member.value for member in CameraType]
-		# self.camera_types = ["Smartphone","BBlock","CineStyle","Handheld Camcorder","Minicam","Minicam Motorizable","Minicam IZT","Mirrorless","PTZ","Shoulder Camcorder","Slow Motion","System","TBD"]
 		self.camera_categories = [member.value for member in CameraCategory]
-		# self.camera_categories = ['Broadcast','Cine Interchangeable','IZF Integrated','Fixed Lens','Minicam Motorizable Lens','TBD' ]
-		# User options of IZF control per camera category
-		self.options_needs_lensControl={
-			 'Broadcast':['No Need','Iris','IZF'],
-			 'Cine Interchangeable':['No Need','Iris','IZF'],
-			 'IZF Integrated':['IZF'],
-			 'Fixed Lens':['No Need'],
-			 'Minicam Motorizable Lens':['No Need','IZF'],
-			 'TBD' :["No Need"]
-		}
-		# User options of lens type per camera category
-		self.options_needs_lensType={
-			 'Broadcast':['B4-Mount'],
-			 'Cine Interchangeable':['B4-Mount','E-Mount','Cabrio','Cineservo','Primelens','Motorized Others','TBD'],
-			 'IZF Integrated':['Camera Integrated'],
-			 'Fixed Lens':['Manual'], # "Manual" only ?
-			 'Minicam Motorizable Lens':['Manual'],
-			 'TBD' :["No Need"]
-		}
-		# User options of motor type per camera category
-		self.options_needs_motorType={
-			 'Broadcast':['No extra motors'],
-			 'Cine Interchangeable':['No extra motors','Tilta','Arri','TBD'],
-			 'IZF Integrated':['Camera Integrated'],
-			 'Fixed Lens':['Manual'], # "Manual" only ?
-			 'Minicam Motorizable Lens':['No extra motors','Dreamchip'],
-			 'TBD' :['No extra motors']
-		}
-		# Initial values for user options per camera category (lensControl,lensType,motorType)
-		self.options_needs_init = {
-			"Broadcast" : ('No Need','B4-Mount','No extra motors'),
-			"Cine Interchangeable" : ('No Need','TBD','TBD'),
-			"IZF Integrated" : ('IZF','Camera Integrated','No extra motors'),
-			"Fixed Lens" : ("No Need",'Manual','No extra motors'),
-			'Minicam Motorizable Lens' : ("No Need",'Manual','No extra motors'),
-			"TBD" : ("No Need",'Manual','No extra motors')
-		}
-				# case "Broadcast": return ('No Need','B4-Mount','No extra motors')
-				# case "Cine Interchangeable": return ('No Need','TBD','TBD')
-				# case "IZF Integrated": return ('IZF','Camera Integrated','No extra motors')
-				# case "Fixed Lens": return ("No Need",'Fixed and Manual','No extra motors')
-				# case "TBD": return ("No Need",'Fixed and Manual','No extra motors')    
-	# Select camera cable + lens cable + lens motor from user needs in Cyanview control
 	@classmethod
 	def adapters(self,parameters):
 		# GET LIST OF ADAPTERS FOR A GIVEN COMBINATION OF LENS CONTROL, LENS TYPE AND LENS MOTOR
